6b0e7f268df42d2978803077bdb521bb515b5fb9
Satsangi, Deepak <Deepak_Satsangi@cable.comcast.com>
2022-02-05 11:09:21 -0500

    changes for v3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to Auto trading app</h1>
+        <h1>Welcome to Auto trading app.</h1>
+        <h2>The app will automatically do a paper trade</h2>
       </header>
     </div>
   );
