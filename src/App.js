f8e7f7fc8c39af206797eb132da536f28156e661
Satsangi, Deepak <Deepak_Satsangi@cable.comcast.com>
2022-02-05 11:44:53 -0500

    changes for v8

--- src/App.js
+++ src/App.js
@@ -8,9 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to Auto trading app.</h1>
-        <h2>The app will automatically do a paper trade</h2>
-        <h3>The app would be available soon for your review! - powered by Deepak Satsangi</h3>
-      </header>
+        </header>
+        <div className="body">
+          <h2>The app will automatically do a paper trade</h2>
+          <h3>The app would be available soon for your review!</h3>
+          <a href="https://byteinnovators.godaddysites.com/">powered by byteinnovators LLC</a>
+        </div>
     </div>
   );
 }
